fcb69093e9c2a553a8cba2a946d6e9a2bd7b1c38
Kowshique Roy <34647424+kowshiqueroy@users.noreply.github.com>
2021-11-06 02:36:00 +0600

    not working

--- ChasE/app/src/main/java/com/example/chase/MainActivity.java
+++ ChasE/app/src/main/java/com/example/chase/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.chase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button connect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        connect= findViewById(R.id.connect);
+
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, BluetoothPage.class));
+            }
+        });
+
     }
 }
