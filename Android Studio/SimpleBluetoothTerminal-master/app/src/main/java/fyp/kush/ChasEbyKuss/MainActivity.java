d39edf04eaac90b7d29f37ec79422fcf0fc39ebb
Kowshique Roy <34647424+kowshiqueroy@users.noreply.github.com>
2021-12-02 23:20:25 +0600

    login

--- Android Studio/SimpleBluetoothTerminal-master/app/src/main/java/fyp/kush/ChasEbyKuss/MainActivity.java
+++ Android Studio/SimpleBluetoothTerminal-master/app/src/main/java/fyp/kush/ChasEbyKuss/MainActivity.java
@@ -1,5 +1,6 @@
 package fyp.kush.ChasEbyKuss;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,12 +10,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import fyp.kush.ChasEbyKuss.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,12 +56,28 @@ int vari=0;
             @Override
             public void onClick(View view) {
 
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+
                 member.setName("Kowshique");
                 member.setEmail("Data"+ vari);
                 vari++;
 
 
                 reff.child(member.getEmail()).setValue(member);
+                reff.child(member.getEmail()).child("name").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if (!task.isSuccessful()) {
+                            Log.e("firebase", "Error getting data", task.getException());
+                        }
+                        else {
+                            Log.d("firebase Get Data: ", String.valueOf(task.getResult().getValue()));
+                        }
+                    }
+                });
 
                 //reff.push().setValue(member.getName() + " " + member.getEmail());
                 //reff.push().setValue("Hello");
