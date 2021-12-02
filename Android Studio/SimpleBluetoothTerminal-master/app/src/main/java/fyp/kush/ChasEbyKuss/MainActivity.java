a367937d484c3fb40718e1bc585dea2f9a7aa0c2
Kowshique Roy <34647424+kowshiqueroy@users.noreply.github.com>
2021-12-02 22:42:34 +0600

    fire

--- Android Studio/SimpleBluetoothTerminal-master/app/src/main/java/fyp/kush/ChasEbyKuss/MainActivity.java
+++ Android Studio/SimpleBluetoothTerminal-master/app/src/main/java/fyp/kush/ChasEbyKuss/MainActivity.java
@@ -4,10 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+
+DatabaseReference reff;
+Member member;
+int vari=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +30,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
+        Button cloudBtn=findViewById(R.id.btncloud);
         Button connect=findViewById(R.id.btnonnect);
+        member= new Member();
+        reff= FirebaseDatabase.getInstance().getReference().child("Member");
+
+
 
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +45,34 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        cloudBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                member.setName("Kowshique");
+                member.setEmail("Data"+ vari);
+                vari++;
+
+
+                reff.child(member.getEmail()).setValue(member);
+
+                //reff.push().setValue(member.getName() + " " + member.getEmail());
+                //reff.push().setValue("Hello");
+                //reff.setValue("Hi");
+                //reff.setValue("Hi2");
+
+
+
+
+
+            }
+        });
+
+
+
+
 
 
     }
---
+++ Android Studio/SimpleBluetoothTerminal-master/app/src/main/java/fyp/kush/ChasEbyKuss/Member.java
@@ -0,0 +1,31 @@
+package fyp.kush.ChasEbyKuss;
+
+import android.provider.ContactsContract;
+
+public class Member {
+    private  String Name;
+    private String email;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Member() {
+
+
+    }
+
+
+}
